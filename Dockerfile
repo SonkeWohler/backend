0bca856fafaf460a5f11c0b108b8e1c30c5023f3
sun <sonke.a.wohler@gmail.com>
2022-01-29 01:27:47 +0200

    make flask work in docker

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+### --- environment
+FROM python:3
+COPY . .
+RUN pip install --no-cache-dir -r requirements.txt
+# I tried "flask run", but that didn't seem to work
+# Wonder why.  Probably the way flask is installed?
+CMD ["python", "./app.py"]
--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
-
 app = Flask(__name__)
+
+
+@app.route('/')
+def rude():
+    return "Rude!  You could say /hello first, you know."
+
 
 @app.route('/hello')
 def hello_world():
-    return { "message": "Hello World" }
+    return {"message": "Hello World"}
+
+
+if __name__ == '__main__':
+    # docker only publishes this ip, not sure yet how it is with expose
+    app.run(host='0.0.0.0', port=5000)
